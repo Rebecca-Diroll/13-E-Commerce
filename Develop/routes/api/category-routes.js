b53fd67fa53b361cd259f7a69cd155358a89697e
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-27 23:29:36 -0400

    Update the first section of category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,8 +4,14 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  try {
+    const catagoryData = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
